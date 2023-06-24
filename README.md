a3e4f480cadf7abf00769edc98aba7887cf454e0
CelestialChewi <pheikhoon@gmail.com>
2023-06-24 14:57:35 +0800

    Trying to use json file instead of text but still working on it

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 1. Building this in PyCharm.
 2. Just an offline password manager to save your password instead of using Google which prone to security breach
 
+
+In the UI Setup, you can choose your desired "logo" and makes sure the logo file name (eg.logo.png) is named correctly into the code too
+
+In line 42, it supposed to auto create a file if you dont have the file in the first place. It does show and contains the data you have put into but it doesnt show up in your project directory view so you can manually create it if you encounter this issue
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import re
+import json
 
 # Password Generator Function
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -25,6 +26,12 @@ def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(email) < 11 or len(password) == 0:
         messagebox.showinfo(title="Warning", message="Please complete the details")
@@ -32,14 +39,22 @@ def save_password():
     elif not validate_email(email):
         messagebox.showinfo(title="Warning", message="Please input the correct email address")
     else:
-        is_correct = messagebox.askyesno(title="Password Manager", message=(f"Are these correct details?\n\n Website: {website}\n Email: {email}\n Password: {password}"))
+        try:
+            with open("password_file.json", "r") as pf:
+                data = json.load(pf) # Read old data
+                data.update(new_data) # Update old data with new data
 
-        if is_correct:
-            with open("password_file.txt", "a") as pf:
-                pf.write(f"{website} | {email} | {password}\n")
+        except FileNotFoundError:
+            open("password_file.json", "r")
+
+        else:
+            with open("password_file.json", "w") as pf:
+                json.dump(new_data, pf, indent=2) # Saving updated data
+
                 website_entry.delete(0, END)
                 email_entry.delete(0, email_entry.index(END)-10)
                 password_entry.delete(0, END)
+                messagebox.showinfo(title="Confirmation", message="Data has added to the database")
 
 # Email Verification
 def validate_email(email):
@@ -61,8 +76,8 @@ canvas.grid(row=0, column=1)
 # Labels and Entries UI Setup
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
-website_entry = Entry(width=41)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=25)
+website_entry.grid(row=1, column=1)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
@@ -74,6 +89,9 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 password_entry = Entry(width=25)
 password_entry.grid(row=3, column=1)
+
+search_button = Button(text="Search", width=15)
+search_button.grid(row=1, column=2)
 
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)

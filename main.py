43f515144182369c461863b3bb5726f076b59a08
CelestialChewi <pheikhoon@gmail.com>
2023-06-24 16:18:11 +0800

    Remove the email verification and added a dropdown list for email domain to choose from. Also added search function to check the email and password for it

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def generate_password():
 # Save Password Function
 def save_password():
     website = website_entry.get()
-    email = email_entry.get()
+    email = email_entry.get() + dropdown_option.get()
     password = password_entry.get()
     new_data = {
         website: {
@@ -33,36 +33,44 @@ def save_password():
         }
     }
 
-    if len(website) == 0 or len(email) < 11 or len(password) == 0:
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showinfo(title="Warning", message="Please complete the details")
-        validate_email(email)
-    elif not validate_email(email):
-        messagebox.showinfo(title="Warning", message="Please input the correct email address")
     else:
         try:
             with open("password_file.json", "r") as pf:
                 data = json.load(pf) # Read old data
-                data.update(new_data) # Update old data with new data
-
         except FileNotFoundError:
-            open("password_file.json", "w")
-
+            with open("password_file.json", "w") as pf: # Create the file
+                json.dump(new_data, pf, indent=2) # Update the old data with new data
         else:
+            data.update(new_data)  # Update old data with new data
+
             with open("password_file.json", "w") as pf:
-                json.dump(new_data, pf, indent=2) # Saving updated data
+                json.dump(data, pf, indent=2) # Saving updated data
+        finally:
+            website_entry.delete(0, END)
+            email_entry.delete(0, email_entry.index(END)-10)
+            password_entry.delete(0, END)
+            messagebox.showinfo(title="Confirmation", message="Data has added to the database")
 
-                website_entry.delete(0, END)
-                email_entry.delete(0, email_entry.index(END)-10)
-                password_entry.delete(0, END)
-                messagebox.showinfo(title="Confirmation", message="Data has added to the database")
-
-# Email Verification
-def validate_email(email):
-    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-    if(re.fullmatch(regex, email)):
-        return True
+# Search Password based on Website
+def search_password():
+    website = website_entry.get()
+    if len(website) == 0:
+        messagebox.showinfo(title="Warning", message="Please enter a website name")
     else:
-        return False
+        try:
+            with open("password_file.json") as pf:
+                data = json.load(pf)
+        except FileNotFoundError:
+            messagebox.showinfo(title="Error", message="No Data Found")
+        else:
+            if website in data:
+                email = data[website]["email"]
+                password = data[website]["password"]
+                messagebox.showinfo(title=website, message=(f"Email: {email}\nPassword: {password}"))
+            else:
+                messagebox.showinfo(title="Error", message=(f"'{website}' not exists in the database"))
 
 # UI Setup
 window = Tk()
@@ -81,23 +89,28 @@ website_entry.grid(row=1, column=1)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
-email_entry = Entry(width=41)
-email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "@gmail.com")
+email_entry = Entry(width=25)
+email_entry.grid(row=2, column=1)
+# email_entry.insert(0, "@gmail.com")
 
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 password_entry = Entry(width=25)
 password_entry.grid(row=3, column=1)
 
-search_button = Button(text="Search", width=15)
-search_button.grid(row=1, column=2)
+search_password_button = Button(text="Search", width=15, command=search_password)
+search_password_button.grid(row=1, column=2)
 
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=41, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2)
+
+dropdown_option = StringVar()
+dropdown_option.set("@gmail.com")
+email_domain_dropdown_menu = OptionMenu(window, dropdown_option, "@gmail.com", "@hotmail.com", "@outlook.com", "@yahoo.com")
+email_domain_dropdown_menu.grid(row=2, column=2)
 
 
 
